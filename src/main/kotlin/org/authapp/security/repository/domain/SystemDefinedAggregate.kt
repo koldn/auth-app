de0cba386da0eaca4adf25c751b3be700ea16d44
koldn <martyr08@gmail.com>
2020-07-24 00:43:59 +0500

    Correct user role aggregate id

--- src/main/kotlin/org/authapp/security/repository/domain/SystemDefinedAggregate.kt
+++ src/main/kotlin/org/authapp/security/repository/domain/SystemDefinedAggregate.kt
@@ -5,5 +5,5 @@ import org.authapp.security.user.role.SystemDefinedRoles
 object SystemDefinedAggregate {
     val ADMIN_ROLE_AGGREGATE = RoleAggregate("admin", setOf(SystemDefinedRoles.ADMIN, SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
     val REVIEWER_ROLE_AGGREGATE = RoleAggregate("reviewer", setOf(SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
-    val USER_ROLE_AGGREGATE = RoleAggregate("reviewer", setOf(SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
+    val USER_ROLE_AGGREGATE = RoleAggregate("user", setOf(SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
 }
