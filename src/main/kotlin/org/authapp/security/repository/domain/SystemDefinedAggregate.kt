950f77e132d4bbf5acc40001e886155f7849f82a
koldn <martyr08@gmail.com>
2020-07-24 00:51:44 +0500

    Imports in SystemDefinedAggregate

--- src/main/kotlin/org/authapp/security/repository/domain/SystemDefinedAggregate.kt
+++ src/main/kotlin/org/authapp/security/repository/domain/SystemDefinedAggregate.kt
@@ -1,9 +1,11 @@
 package org.authapp.security.repository.domain
 
-import org.authapp.security.user.role.SystemDefinedRoles
+import org.authapp.security.user.role.SystemDefinedRoles.ADMIN
+import org.authapp.security.user.role.SystemDefinedRoles.REVIEWER
+import org.authapp.security.user.role.SystemDefinedRoles.USER
 
 object SystemDefinedAggregate {
-    val ADMIN_ROLE_AGGREGATE = RoleAggregate("admin", setOf(SystemDefinedRoles.ADMIN, SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
-    val REVIEWER_ROLE_AGGREGATE = RoleAggregate("reviewer", setOf(SystemDefinedRoles.REVIEWER, SystemDefinedRoles.USER))
-    val USER_ROLE_AGGREGATE = RoleAggregate("user", setOf(SystemDefinedRoles.USER))
+    val ADMIN_ROLE_AGGREGATE = RoleAggregate("admin", setOf(ADMIN, REVIEWER, USER))
+    val REVIEWER_ROLE_AGGREGATE = RoleAggregate("reviewer", setOf(REVIEWER, USER))
+    val USER_ROLE_AGGREGATE = RoleAggregate("user", setOf(USER))
 }
