0a892bb2e0f302c3d7b25a7613c5967ffe99c742
koldn <martyr08@gmail.com>
2020-07-26 16:02:11 +0500

    Single application for all tests

--- src/test/kotlin/org/authapp/security/auth/AuthenticationTest.kt
+++ src/test/kotlin/org/authapp/security/auth/AuthenticationTest.kt
@@ -1,35 +1,51 @@
 package org.authapp.security.auth
 
-import io.ktor.application.Application
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.createTestEnvironment
 import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.withTestApplication
 import org.authapp.configureApplication
 import org.authapp.security.encrypt.PasswordCoder
 import org.authapp.security.repository.DataRepository
 import org.authapp.security.repository.domain.DomainUser
 import org.authapp.security.repository.domain.SystemDefinedAggregate
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.kodein.di.instance
 import org.kodein.di.ktor.di
 import java.nio.charset.StandardCharsets
 import java.util.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthenticationTest {
+    lateinit var engine: TestApplicationEngine
+
+    @BeforeAll
+    fun startServer() {
+        val environment = createTestEnvironment()
+        val testApplicationEngine = TestApplicationEngine(environment)
+        testApplicationEngine.start()
+        testApplicationEngine.application.configureApplication()
+        engine = testApplicationEngine
+    }
+
+    @AfterAll
+    internal fun stopServer() {
+        engine.stop(0L, 0L)
+    }
+
     @Test
-    fun `Request without authorization header should be responded with 'Unauthorized' status code`(): Unit = withTestApplication(Application::configureApplication) {
-        with(handleRequest(HttpMethod.Post, "/authenticate")) {
+    fun `Request without authorization header should be responded with 'Unauthorized' status code`() {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate")) {
             Assertions.assertEquals(HttpStatusCode.Unauthorized, response.status())
         }
     }
 
     @Test
-    fun `Unauthorized when user is not found`(): Unit = withTestApplication(Application::configureApplication) {
+    fun `Unauthorized when user is not found`() {
         val password = Base64.getEncoder().encodeToString("test:test1".toByteArray(StandardCharsets.UTF_8))
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $password")
         }) {
             Assertions.assertEquals(HttpStatusCode.Unauthorized, response.status())
@@ -37,12 +53,12 @@ class AuthenticationTest {
     }
 
     @Test
-    fun `Token should be handled to found user`(): Unit = withTestApplication(Application::configureApplication) {
-        val userRepository by application.di().instance<DataRepository<DomainUser>>()
-        val passwordCoder by application.di().instance<PasswordCoder>()
+    fun `Token should be handled to found user`() {
+        val userRepository by engine.application.di().instance<DataRepository<DomainUser>>()
+        val passwordCoder by engine.application.di().instance<PasswordCoder>()
         userRepository.save(DomainUser("test", passwordCoder.encodePassword("12345"), ""))
         val credentials = Base64.getEncoder().encodeToString("test:12345".toByteArray(StandardCharsets.UTF_8))
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $credentials")
         }) {
             Assertions.assertEquals(HttpStatusCode.OK, response.status())
@@ -51,20 +67,20 @@ class AuthenticationTest {
     }
 
     @Test
-    fun `Unauthorized on admin space with reviewer role`(): Unit = withTestApplication(Application::configureApplication) {
-        val userRepository by application.di().instance<DataRepository<DomainUser>>()
-        val passwordCoder by application.di().instance<PasswordCoder>()
+    fun `Unauthorized on admin space with reviewer role`() {
+        val userRepository by engine.application.di().instance<DataRepository<DomainUser>>()
+        val passwordCoder by engine.application.di().instance<PasswordCoder>()
         userRepository.save(DomainUser("test", passwordCoder.encodePassword("12345"), SystemDefinedAggregate.REVIEWER_ROLE_AGGREGATE.aggregateId))
         val credentials = Base64.getEncoder().encodeToString("test:12345".toByteArray(StandardCharsets.UTF_8))
 
         val token: String
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $credentials")
         }) {
             token = response.content!!
         }
 
-        with(handleRequest(HttpMethod.Post, "/admin_space") {
+        with(engine.handleRequest(HttpMethod.Post, "/admin_space") {
             addHeader(HttpHeaders.Authorization, "Bearer $token")
         }) {
             Assertions.assertEquals(HttpStatusCode.Unauthorized, response.status())
@@ -73,20 +89,20 @@ class AuthenticationTest {
     }
 
     @Test
-    fun `Unauthorized on reviewer space with user role`(): Unit = withTestApplication(Application::configureApplication) {
-        val userRepository by application.di().instance<DataRepository<DomainUser>>()
-        val passwordCoder by application.di().instance<PasswordCoder>()
+    fun `Unauthorized on reviewer space with user role`() {
+        val userRepository by engine.application.di().instance<DataRepository<DomainUser>>()
+        val passwordCoder by engine.application.di().instance<PasswordCoder>()
         userRepository.save(DomainUser("test", passwordCoder.encodePassword("12345"), SystemDefinedAggregate.USER_ROLE_AGGREGATE.aggregateId))
         val credentials = Base64.getEncoder().encodeToString("test:12345".toByteArray(StandardCharsets.UTF_8))
 
         val token: String
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $credentials")
         }) {
             token = response.content!!
         }
 
-        with(handleRequest(HttpMethod.Post, "/reviewer_space") {
+        with(engine.handleRequest(HttpMethod.Post, "/reviewer_space") {
             addHeader(HttpHeaders.Authorization, "Bearer $token")
         }) {
             Assertions.assertEquals(HttpStatusCode.Unauthorized, response.status())
@@ -95,20 +111,20 @@ class AuthenticationTest {
     }
 
     @Test
-    fun `Higher role has access`(): Unit = withTestApplication(Application::configureApplication) {
-        val userRepository by application.di().instance<DataRepository<DomainUser>>()
-        val passwordCoder by application.di().instance<PasswordCoder>()
+    fun `Higher role has access`() {
+        val userRepository by engine.application.di().instance<DataRepository<DomainUser>>()
+        val passwordCoder by engine.application.di().instance<PasswordCoder>()
         userRepository.save(DomainUser("test", passwordCoder.encodePassword("12345"), SystemDefinedAggregate.ADMIN_ROLE_AGGREGATE.aggregateId))
         val credentials = Base64.getEncoder().encodeToString("test:12345".toByteArray(StandardCharsets.UTF_8))
 
         val token: String
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $credentials")
         }) {
             token = response.content!!
         }
 
-        with(handleRequest(HttpMethod.Post, "/reviewer_space") {
+        with(engine.handleRequest(HttpMethod.Post, "/reviewer_space") {
             addHeader(HttpHeaders.Authorization, "Bearer $token")
         }) {
             Assertions.assertEquals("Hello reviewer!", response.content)
@@ -116,20 +132,20 @@ class AuthenticationTest {
     }
 
     @Test
-    fun `User role has access to user space`(): Unit = withTestApplication(Application::configureApplication) {
-        val userRepository by application.di().instance<DataRepository<DomainUser>>()
-        val passwordCoder by application.di().instance<PasswordCoder>()
+    fun `User role has access to user space`() {
+        val userRepository by engine.application.di().instance<DataRepository<DomainUser>>()
+        val passwordCoder by engine.application.di().instance<PasswordCoder>()
         userRepository.save(DomainUser("test", passwordCoder.encodePassword("12345"), SystemDefinedAggregate.USER_ROLE_AGGREGATE.aggregateId))
         val credentials = Base64.getEncoder().encodeToString("test:12345".toByteArray(StandardCharsets.UTF_8))
 
         val token: String
-        with(handleRequest(HttpMethod.Post, "/authenticate") {
+        with(engine.handleRequest(HttpMethod.Post, "/authenticate") {
             addHeader(HttpHeaders.Authorization, "Basic $credentials")
         }) {
             token = response.content!!
         }
 
-        with(handleRequest(HttpMethod.Post, "/user_space") {
+        with(engine.handleRequest(HttpMethod.Post, "/user_space") {
             addHeader(HttpHeaders.Authorization, "Bearer $token")
         }) {
             Assertions.assertEquals("Hello user!", response.content)
